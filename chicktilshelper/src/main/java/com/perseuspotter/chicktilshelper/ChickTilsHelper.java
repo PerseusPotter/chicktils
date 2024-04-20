ba5cf2f454f3f2d16dfbe0da0da69df283934d6c
Ziyang Qiu <danktha0s@gmail.com>
2024-04-19 23:44:29 -0400

    auto update system

--- chicktilshelper/src/main/java/com/perseuspotter/chicktilshelper/ChickTilsHelper.java
+++ chicktilshelper/src/main/java/com/perseuspotter/chicktilshelper/ChickTilsHelper.java
@@ -1,7 +1,12 @@
 package com.perseuspotter.chicktilshelper;
 
 import java.lang.reflect.Field;
+import java.net.URLConnection;
 import java.util.List;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ChatLine;
 import net.minecraft.client.gui.GuiNewChat;
@@ -60,5 +65,32 @@ public class ChickTilsHelper {
     } catch (IllegalAccessException e) {} catch (
       NoSuchFieldException e
     ) {} catch (NullPointerException e) {}
+  }
+
+  // https://stackoverflow.com/a/2752455
+  private static final TrustManager[] trustAllCerts = new TrustManager[] {
+    new X509TrustManager() {
+      public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
+
+      public void checkClientTrusted(
+        java.security.cert.X509Certificate[] certs,
+        String authType
+      ) {}
+
+      public void checkServerTrusted(
+        java.security.cert.X509Certificate[] certs,
+        String authType
+      ) {}
+    },
+  };
+
+  public static void removeCertCheck(URLConnection url) {
+    try {
+      SSLContext sc = SSLContext.getInstance("SSL");
+      sc.init(null, trustAllCerts, null);
+      ((HttpsURLConnection) url).setSSLSocketFactory(sc.getSocketFactory());
+    } catch (Exception e) {}
   }
 }
