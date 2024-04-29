45631339ad8dd17d63d8a9d50c7acd2afb1cdb35
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-04-29 05:53:11 -0400

    ping fixes

--- util/ping.js
+++ util/ping.js
@@ -4,7 +4,7 @@ import settings from '../settings';
 const Ping = Java.type('gg.skytils.skytilsmod.features.impl.misc.Ping');
 let lastPing = 0;
 export default function getPing() {
-  if (!Ping) return 0;
+  if (!Ping || settings.pingRefreshDelay === 0) return 0;
   const t = Date.now();
   if (t - lastPing > settings.pingRefreshDelay * 1000) {
     Ping.INSTANCE.sendPing();
