caefbf8c977026844e75ad79eddb1d3d45c6efd1
Ch1ck3nNeedsRNG <chick_is_bored>
2024-05-11 20:38:47 -0400

    b

--- util/ping.js
+++ util/ping.js
@@ -7,7 +7,7 @@ export default function getPing() {
   if (!Ping || settings.pingRefreshDelay === 0) return 0;
   const t = Date.now();
   if (t - lastPing > settings.pingRefreshDelay * 1000) {
-    Ping.INSTANCE.sendPing();
+    new Thread(() => Ping.INSTANCE.sendPing()).start();
     lastPing = t;
   }
   return Ping.INSTANCE.getPingCache();
