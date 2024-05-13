af3cc2059ac527298a70596fc56dd784103d644a
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-05-13 19:01:19 -0400

    + multiple grid options

--- util/grid.js
+++ util/grid.js
@@ -3,10 +3,10 @@ import { log } from './log';
 export default class Grid {
   size = 0;
   key = 0;
-  addNeighbors = false;
+  addNeighbors = 0;
   hm = new (Java.type('java.util.concurrent.ConcurrentHashMap'))();
   arrs = [];
-  constructor({ size = 1, key = 631, addNeighbors = false } = {}) {
+  constructor({ size = 1, key = 631, addNeighbors = 0 } = {}) {
     this.size = size;
     this.key = key;
     this.addNeighbors = addNeighbors;
@@ -32,14 +32,16 @@ export default class Grid {
   add(x, z, item) {
     const id = this._getId(x, z);
     this._addWithId(id, item);
-    if (this.addNeighbors) {
-      this._addWithId(id - this.key - 1, item);
+    if (this.addNeighbors > 0) {
       this._addWithId(id - this.key + 0, item);
-      this._addWithId(id - this.key + 1, item);
       this._addWithId(id - 1, item);
       this._addWithId(id + 1, item);
-      this._addWithId(id + this.key - 1, item);
       this._addWithId(id + this.key + 0, item);
+    }
+    if (this.addNeighbors === 2) {
+      this._addWithId(id - this.key - 1, item);
+      this._addWithId(id - this.key + 1, item);
+      this._addWithId(id + this.key - 1, item);
       this._addWithId(id + this.key + 1, item);
     }
   }
