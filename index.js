23bbd2fd50eb5568d284665c037dc10260ef5a36
Ch1ck3nNeedsRNG <chick_is_bored>
2024-05-18 17:36:18 -0400

    v0.2.11

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { centerMessage } from './util/format';
 import getPing from './util/ping';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.2.10';
+const VERSION = '0.2.11';
 
 let sev;
 function tryUpdate(delay = 0) {
