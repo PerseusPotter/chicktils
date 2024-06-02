649dbffb5f76586f43941cd35b5d1f38b709d949
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-02 05:08:05 -0400

    v0.3.1

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { centerMessage } from './util/format';
 import getPing from './util/ping';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.3.0';
+const VERSION = '0.3.1';
 
 let sev;
 function tryUpdate(delay = 0) {
