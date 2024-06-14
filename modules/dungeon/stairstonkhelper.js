36e5b6397c0ffd959013746f4efb07b20649dfd1
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-13 20:10:27 -0400

    highlight stairs to be ghosted

--- modules/dungeon/stairstonkhelper.js
+++ modules/dungeon/stairstonkhelper.js
@@ -1,13 +1,18 @@
 import settings from '../../settings';
-import { drawLine3D } from '../../util/draw';
+import { drawFilledBox, drawLine3D } from '../../util/draw';
 import reg from '../../util/registerer';
 import Grid from '../../util/grid';
+import { getBlockPos } from '../../util/mc';
 
 const brokenStairBucket = new Grid({ size: 2, addNeighbors: 2 });
 let cID = NaN;
 let cArr = null;
+let lookPos = null;
 
 const BlockStairs = Java.type('net.minecraft.block.BlockStairs');
+const MovingObjectPosition = Java.type('net.minecraft.util.MovingObjectPosition');
+
+// curved stairs arent a thing :pray:
 const stairBreakReg = reg('blockBreak', b => {
   if (!(b.type.mcBlock instanceof BlockStairs)) return;
   const x = b.getX();
@@ -18,16 +23,16 @@ const stairBreakReg = reg('blockBreak', b => {
   cID = NaN;
   switch (b.getMetadata()) {
     case 0:
-      brokenStairBucket.add(x, z, [n, [x + 0.24, y + 1.1, z], [x + 0.24, y + 1.1, z + 1]]);
+      brokenStairBucket.add(x, z, [n, [x + 0.24, y + 1.02, z], [x + 0.24, y + 1.02, z + 1]]);
       break;
     case 1:
-      brokenStairBucket.add(x, z, [n, [x + 0.76, y + 1.1, z], [x + 0.76, y + 1.1, z + 1]]);
+      brokenStairBucket.add(x, z, [n, [x + 0.76, y + 1.02, z], [x + 0.76, y + 1.02, z + 1]]);
       break;
     case 2:
-      brokenStairBucket.add(x, z, [n, [x, y + 1.1, z + 0.24], [x + 1, y + 1.1, z + 0.24]]);
+      brokenStairBucket.add(x, z, [n, [x, y + 1.02, z + 0.24], [x + 1, y + 1.02, z + 0.24]]);
       break;
     case 3:
-      brokenStairBucket.add(x, z, [n, [x, y + 1.1, z + 0.76], [x + 1, y + 1.1, z + 0.76]]);
+      brokenStairBucket.add(x, z, [n, [x, y + 1.02, z + 0.76], [x + 1, y + 1.02, z + 0.76]]);
       break;
   }
 }, 'dungeon/stairstonkhelper').setEnabled(settings._dungeonStairStonkHelper);
@@ -37,6 +42,19 @@ const tickReg = reg('tick', () => {
     cID = id;
     cArr = brokenStairBucket._getById(id);
   }
+
+  lookPos = null;
+  const obj = Client.getMinecraft().field_71476_x;
+  if (obj === null || obj.field_72313_a !== MovingObjectPosition.MovingObjectType.BLOCK) return;
+  const bPos = obj.func_178782_a();
+  if (bPos === null) return;
+
+  const bState = World.getWorld().func_180495_p(bPos);
+  const b = bState.func_177230_c();
+  if (!(b instanceof BlockStairs)) return;
+  const m = b.func_176201_c(bState);
+  if (m === 1 || m === 3) return;
+  lookPos = getBlockPos(bPos);
 }, 'stairstonkhelper');
 const renderWorldReg = reg('renderWorld', () => {
   if (!cArr) {
@@ -55,6 +73,14 @@ const renderWorldReg = reg('renderWorld', () => {
       2
     );
   });
+
+  if (lookPos) {
+    const r = ((settings.dungeonStairStonkHelperHighlightColor >> 24) & 0xFF) / 256;
+    const g = ((settings.dungeonStairStonkHelperHighlightColor >> 16) & 0xFF) / 256;
+    const b = ((settings.dungeonStairStonkHelperHighlightColor >> 8) & 0xFF) / 256;
+    const a = ((settings.dungeonStairStonkHelperHighlightColor >> 0) & 0xFF) / 256;
+    drawFilledBox(lookPos.x + 0.4995, lookPos.y - 0.005, lookPos.z + 0.4995, 1.01, 1.01, r, g, b, a, false);
+  }
 }, 'dungeon/stairstonkhelper').setEnabled(settings._dungeonStairStonkHelper);
 
 export function init() { }
@@ -62,6 +88,7 @@ export function start() {
   brokenStairBucket.clear();
   cID = NaN;
   cArr = null;
+  lookPos = null;
 
   stairBreakReg.register();
   tickReg.register();
