bbb907e7c05974fe6c91cff17c032fad7de90dcb
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-17 22:05:17 -0400

    v0.4.6

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { getRegs } from './util/registerer';
 import { calcMedian } from './util/math';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.4.5';
+const VERSION = '0.4.6';
 
 let sev;
 function tryUpdate(delay = 0) {
