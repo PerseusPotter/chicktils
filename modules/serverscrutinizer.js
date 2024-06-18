3560c51f2e132eb121209d95143603c9055c24a9
Ch1ck3nNeedsRNG <chick_is_bored>
2024-06-18 03:45:06 -0400

    fix ss

--- modules/serverscrutinizer.js
+++ modules/serverscrutinizer.js
@@ -5,8 +5,8 @@ import { colorForNumber } from '../util/format';
 import { log } from '../util/log';
 import reg from '../util/registerer';
 
-let tpsC = 0;
-let ticks = [Date.now()];
+let ticks = [];
+let lastLoadTime = 0;
 const MAX_TICK_AGE = 5_000;
 const cap = n => settings.serverScrutinizerTPSDisplayCap20 ? Math.min(20, n) : n;
 function getCurrentTPS() {
@@ -19,38 +19,36 @@ function getCurrentTPS() {
 function getAverageTPS() {
   return cap(ticks.length / MAX_TICK_AGE * 1000);
 }
-let minTPSC = 0;
-let minTPSV = -1;
 function getMinimumTPS() {
-  if (tpsC === minTPSV) return minTPSC;
-  minTPSC = _getMinimumTPS();
-  minTPSV = tpsC;
-  return minTPSC;
-}
-function _getMinimumTPS() {
-  if (ticks.length === 0) return 0;
+  if (ticks.length <= 1) return ticks.length;
+  const t = ticks.slice();
   let l = 0;
   let r = 1;
   let m = Number.POSITIVE_INFINITY;
-  while (ticks.length > r) {
-    while (ticks.length > r && ticks[l] - ticks[r] <= 1_000) r++;
+  while (t.length > r) {
+    while (t.length > r && t[l] - t[r] <= 1_000) r++;
     if (r - l < m) m = r - l;
     l++;
   }
   return cap(m);
 }
-
-const serverTickReg = reg('packetReceived', () => {
+function trimTicks() {
   const t = Date.now();
-  ticks.unshift(t);
-  tpsC++;
-}, 'serverscrutinizer').setFilteredClass(Java.type('net.minecraft.network.play.server.S32PacketConfirmTransaction'));
+  while (t - ticks[ticks.length - 1] > MAX_TICK_AGE) ticks.pop();
+}
+
+const serverTickReg = reg('packetReceived', () => ticks.unshift(Date.now()), 'serverscrutinizer').setFilteredClass(Java.type('net.minecraft.network.play.server.S32PacketConfirmTransaction'));
+const worldLoadReg = reg('worldLoad', () => {
+  ticks = [];
+  lastLoadTime = Date.now();
+}, 'serverscrutinizer');
 
 function getTPSColor(tps) {
   return colorForNumber(tps - 15, 5);
 }
 
 const tpsCmd = reg('command', () => {
+  trimTicks();
   const tps = getCurrentTPS();
   log('Current TPS:', getTPSColor(tps) + tps);
   const aTPS = getAverageTPS();
@@ -60,6 +58,7 @@ const tpsCmd = reg('command', () => {
 }, 'serverscrutinizer').setName('tps');
 
 function formatTps(curr, avg, min) {
+  if (Date.now() - lastLoadTime < 11_000) return ['TPS: Loading...'];
   if (settings.serverScrutinizerTPSDisplayCurr + settings.serverScrutinizerTPSDisplayAvg + settings.serverScrutinizerTPSDisplayMin === 1) {
     if (settings.serverScrutinizerTPSDisplayCurr || settings.serverScrutinizerTPSDisplayMin) return ['TPS: ' + getTPSColor(curr) + curr];
     return ['TPS: ' + getTPSColor(avg) + avg.toFixed(1)];
@@ -72,15 +71,17 @@ function formatTps(curr, avg, min) {
 }
 const tpsDisplay = createTextGui(() => data.serverScrutinizerTPSDisplay, () => formatTps(20, 18.4, 11));
 const rendOvTps = reg('renderOverlay', () => {
-  const t = Date.now();
-  while (t - ticks[ticks.length - 1] > MAX_TICK_AGE) ticks.pop();
+  trimTicks();
   tpsDisplay.setLines(formatTps(getCurrentTPS(), getAverageTPS(), getMinimumTPS()));
   tpsDisplay.render();
 }, 'serverscrutinizer').setEnabled(settings._serverScrutinizerTPSDisplay);
 
 const lastTickDisplay = createTextGui(() => data.serverScrutinizerLastPacketDisplay, () => ['zzz for &469.42s']);
 const rendOvLTD = reg('renderOverlay', () => {
-  const t = Date.now() - ticks[0];
+  const d = Date.now();
+  if (d - lastLoadTime < 11_000) return;
+  trimTicks();
+  const t = d - (ticks.length ? ticks[0] : lastLoadTime);
   if (t < settings.serverScrutinizerLastTickThreshold) return;
   lastTickDisplay.setLine(`zzz for ${colorForNumber(2000 - t, 2000)}${(t / 1000).toFixed(2)}s`);
   lastTickDisplay.render();
@@ -94,12 +95,14 @@ export function load() {
   ticks = [];
 
   serverTickReg.register();
+  worldLoadReg.register();
   tpsCmd.register();
   rendOvTps.register();
   rendOvLTD.register();
 }
 export function unload() {
   serverTickReg.unregister();
+  worldLoadReg.unregister();
   tpsCmd.unregister();
   rendOvTps.unregister();
   rendOvLTD.unregister();
