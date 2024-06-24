f8859d50d63b4f031ed9b3f340b358c55415e679
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-24 06:21:16 -0400

    clipboard helper

---
+++ util/clipboard.js
@@ -0,0 +1,28 @@
+const board = Java.type('java.awt.Toolkit').getDefaultToolkit().getSystemClipboard();
+const DataFlavor = Java.type('java.awt.datatransfer.DataFlavor');
+const StringSelection = Java.type('java.awt.datatransfer.StringSelection');
+const Character = Java.type('java.lang.Character');
+const StringBuilder = Java.type('java.lang.StringBuilder');
+/**
+ * @returns {string}
+ */
+export function get() {
+  const t = board.getContents(null);
+  const r = DataFlavor.selectBestTextFlavor(t.getTransferDataFlavors()).getReaderForText(t);
+  const s = new StringBuilder();
+  let b = new Packages.java.lang.reflect.Array.newInstance(Character.TYPE, 65536);
+  let l;
+  while ((l = r.read(b, 0, 65536)) > 0) s.append(b, 0, l);
+  r.close();
+  return s.toString();
+}
+/**
+ * @param {string} v
+ */
+export function set(v) {
+  return board.setContents(new StringSelection(v), null);
+}
+
+export function getImage() {
+  return board.getContents(null).getTransferData(DataFlavor.imageFlavor);
+}
