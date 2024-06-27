bfa42c1b366b270623079a3b0f892c11e0b1afa4
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-27 15:46:36 -0400

    faster tps calc

--- modules/serverscrutinizer.js
+++ modules/serverscrutinizer.js
@@ -8,44 +8,54 @@ import reg from '../util/registerer';
 let ticks = [];
 let lastLoadTime = Date.now();
 let lastTickTime = Date.now();
-const MAX_TICK_AGE = 5_000;
 const cap = n => settings.serverScrutinizerTPSDisplayCap20 ? Math.min(20, n) : n;
-function getCurrentTPS() {
-  if (ticks.length === 0) return 0;
-  const f = Date.now();
-  let i = 0;
-  while (ticks.length > i && f - ticks[i] <= 1_000) i++;
-  return cap(i);
-}
-function getAverageTPS() {
-  return cap(ticks.length / MAX_TICK_AGE * 1000);
-}
-function getMinimumTPS() {
-  if (ticks.length <= 1) return ticks.length;
-  const n = Date.now();
-  let lt = n;
-  let l = 0, r = -1;
-  let m = Number.POSITIVE_INFINITY;
-  while (l < ticks.length && r < ticks.length - 1) {
-    while (r < ticks.length - 1 && lt - ticks[r + 1] <= 1_000) r++;
-    let d = r - l + 1;
-    if (
-      r === l &&
-      (
-        r < ticks.length - 1 && ticks[r] - ticks[r + 1] > 1_000 ||
-        r > 0 && ticks[r - 1] - ticks[r] > 1_000 ||
-        r === 0 && n - ticks[r] > 1_000
-      )
-    ) d = 0;
-    m = Math.min(m, d);
-    l++;
-    lt = ticks[l];
-  }
-  return cap(m);
-}
 function trimTicks() {
   const t = Date.now();
-  while (t - ticks[ticks.length - 1] > MAX_TICK_AGE) ticks.pop();
+  while (t - ticks[ticks.length - 1] > settings.serverScrutinizerTPSMaxAge) ticks.pop();
+}
+let lTps = 0;
+let rTps = 0;
+let cTps = 0;
+let aTps = 0;
+let mTps = 0;
+function calcTps() {
+  if (ticks.length === 0) {
+    cTps = 0;
+    aTps = 0;
+    mTps = 0;
+    return;
+  }
+  if (ticks[0] === lTps && ticks[ticks.length - 1] === rTps) return;
+  lTps = ticks[0];
+  rTps = ticks[ticks.length - 1];
+  const d = Date.now();
+
+  cTps = 0;
+  while (ticks.length > cTps && d - ticks[cTps] <= 1_000) cTps++;
+
+  aTps = ticks.length / settings.serverScrutinizerTPSMaxAge * 1000;
+
+  if (ticks.length === 1) mTps = 1;
+  else {
+    let lt = d;
+    let l = 0, r = -1;
+    mTps = Number.POSITIVE_INFINITY;
+    while (l < ticks.length && r < ticks.length - 1) {
+      while (r < ticks.length - 1 && lt - ticks[r + 1] <= 1_000) r++;
+      let d = r - l + 1;
+      if (
+        r === l &&
+        (
+          r < ticks.length - 1 && ticks[r] - ticks[r + 1] > 1_000 ||
+          r > 0 && ticks[r - 1] - ticks[r] > 1_000 ||
+          r === 0 && d - ticks[r] > 1_000
+        )
+      ) d = 0;
+      mTps = Math.min(mTps, d);
+      l++;
+      lt = ticks[l];
+    }
+  }
 }
 
 const serverTickReg = reg('packetReceived', () => {
@@ -64,12 +74,10 @@ function getTPSColor(tps) {
 
 const tpsCmd = reg('command', () => {
   trimTicks();
-  const tps = getCurrentTPS();
-  log('Current TPS:', getTPSColor(tps) + tps);
-  const aTPS = getAverageTPS();
-  log('Average TPS:', getTPSColor(aTPS) + aTPS.toFixed(1));
-  const mTPS = getMinimumTPS();
-  log('Minimum TPS:', getTPSColor(mTPS) + mTPS);
+  calcTps();
+  log('Current TPS:', getTPSColor(cTps) + cTps);
+  log('Average TPS:', getTPSColor(aTps) + aTps.toFixed(1));
+  log('Minimum TPS:', getTPSColor(mTps) + mTps);
 }, 'serverscrutinizer').setName('tps');
 
 function formatTps(curr, avg, min) {
@@ -87,7 +95,8 @@ function formatTps(curr, avg, min) {
 const tpsDisplay = createTextGui(() => data.serverScrutinizerTPSDisplay, () => formatTps(20, 18.4, 11));
 const rendOvTps = reg('renderOverlay', () => {
   trimTicks();
-  tpsDisplay.setLines(formatTps(getCurrentTPS(), getAverageTPS(), getMinimumTPS()));
+  calcTps();
+  tpsDisplay.setLines(formatTps(cap(cTps), cap(aTps), cap(mTps)));
   tpsDisplay.render();
 }, 'serverscrutinizer').setEnabled(settings._serverScrutinizerTPSDisplay);
 
