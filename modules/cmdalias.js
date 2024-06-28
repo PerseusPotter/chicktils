a03f230b559879d989501809e8089c55990e0018
Ch1ck3nNeedsRNG <chick_is_bored>
2024-06-27 20:57:38 -0400

    change to use enabled

--- modules/cmdalias.js
+++ modules/cmdalias.js
@@ -5,41 +5,22 @@ import reg from '../util/registerer';
 // rhino :clown:
 // java.lang.IllegalStateException at org.mozilla.javascript.ScriptRuntime.getTopCallScope(ScriptRuntime.java:3926)
 // execCmd.bind(null, ...)
-const storage = new Array(9).fill(0).map((_, i) =>
-  reg('command', execCmd.bind({}, 'enderchest ' + (i + 1)), 'cmdalias').setName('e' + (i + 1), true)
-).concat(new Array(18).fill(0).map((_, i) =>
-  reg('command', execCmd.bind({}, 'backpack ' + (i + 1)), 'cmdalias').setName('b' + (i + 1), true).setAliases('' + (i + 1))
-));
-const names = ['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN'];
-const kuudraNames = ['NORMAL', 'HOT', 'BURNING', 'FIERY', 'INFERNAL'];
-const dungeon = new Array(7).fill(0).map((_, i) =>
-  reg('command', execCmd.bind({}, 'joininstance CATACOMBS_FLOOR_' + names[i]), 'cmdalias').setName('f' + (i + 1), true)
-).concat(new Array(7).fill(0).map((_, i) =>
-  reg('command', execCmd.bind({}, 'joininstance MASTER_CATACOMBS_FLOOR_' + names[i]), 'cmdalias').setName('m' + (i + 1), true)
-));
-dungeon.unshift(reg('command', execCmd.bind({}, 'joininstance CATACOMBS_ENTRANCE'), 'cmdalias').setName('fe', true));
-const kuudra = new Array(5).fill(0).map((_, i) =>
-  reg('command', execCmd.bind({}, 'joininstance KUUDRA_' + kuudraNames[i]), 'cmdalias').setName('k' + (i + 1), true)
-);
+const allRegs = [];
 
-function updater(list) {
-  return function(v) {
-    if (v) list.forEach(v => v.register());
-    else list.forEach(v => v.unregister());
-  }
-}
-export function init() {
-  settings._cmdAliasStorage.onAfterChange(updater(storage));
-  settings._cmdAliasDungeon.onAfterChange(updater(dungeon));
-  settings._cmdAliasKuudra.onAfterChange(updater(kuudra));
-}
+for (let i = 0; i < 9; i++) allRegs.push(reg('command', execCmd.bind({}, 'enderchest ' + (i + 1)), 'cmdalias').setName('e' + (i + 1), true).setEnabled(settings._cmdAliasStorage));
+for (let i = 0; i < 18; i++) allRegs.push(reg('command', execCmd.bind({}, 'backpack ' + (i + 1)), 'cmdalias').setName('b' + (i + 1), true).setAliases('' + (i + 1)).setEnabled(settings._cmdAliasStorage));
+
+allRegs.push(reg('command', execCmd.bind({}, 'joininstance CATACOMBS_ENTRANCE'), 'cmdalias').setName('fe', true).setEnabled(settings._cmdAliasDungeon));
+['ONE', 'TWO', 'THREE', 'FOUR', 'FIVE', 'SIX', 'SEVEN'].forEach((v, i) => {
+  allRegs.push(reg('command', execCmd.bind({}, 'joininstance CATACOMBS_FLOOR_' + v), 'cmdalias').setName('f' + (i + 1), true).setEnabled(settings._cmdAliasDungeon));
+  allRegs.push(reg('command', execCmd.bind({}, 'joininstance MASTER_CATACOMBS_FLOOR_' + v), 'cmdalias').setName('m' + (i + 1), true).setEnabled(settings._cmdAliasDungeon));
+});
+['NORMAL', 'HOT', 'BURNING', 'FIERY', 'INFERNAL'].forEach((v, i) => allRegs.push(reg('command', execCmd.bind({}, 'joininstance KUUDRA_' + v), 'cmdalias').setName('k' + (i + 1), true).setEnabled(settings._cmdAliasKuudra)));
+
+export function init() { }
 export function load() {
-  if (settings.cmdAliasStorage) updater(storage)(true);
-  if (settings.cmdAliasDungeon) updater(dungeon)(true);
-  if (settings.cmdAliasKuudra) updater(kuudra)(true);
+  allRegs.forEach(v => v.register());
 }
 export function unload() {
-  updater(storage)(false);
-  updater(dungeon)(false);
-  updater(kuudra)(false);
+  allRegs.forEach(v => v.unregister());
 }
