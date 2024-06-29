59bf4ef3a4a0d7a2c0273de2161c826a37fed4a3
Ch1ck3nNeedsRNG <chick_is_bored>
2024-06-29 02:40:08 -0400

    MOAR!

--- util/threading.js
+++ util/threading.js
@@ -1,4 +1,4 @@
-const ThreadPool = Java.type('java.util.concurrent.Executors').newFixedThreadPool(4);
+const ThreadPool = Java.type('java.util.concurrent.Executors').newFixedThreadPool(8);
 
 export function run(cb) {
   // org.mozilla.javascript.EvaluatorException: The choice of Java method java.util.concurrent.AbstractExecutorService.submit matching JavaScript argument types (function) is ambiguous; candidate methods are:
