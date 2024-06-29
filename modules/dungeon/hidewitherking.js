24678d7e17a2e59518cbd4c8a2939214197aa0b9
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-06-29 02:40:04 -0400

    hide wither king particles

--- modules/dungeon/hidewitherking.js
+++ modules/dungeon/hidewitherking.js
@@ -6,14 +6,19 @@ import { stateFloor, stateIsInBoss } from '../dungeon.js';
 const entSpawnReg = reg(net.minecraftforge.event.entity.EntityJoinWorldEvent, evn => {
   const e = evn.entity;
   if (e.getClass().getSimpleName() !== 'EntityArmorStand') return;
-  if (e.field_70163_u < 10 || e.field_70163_u > 25 || e.field_70161_v > 45) return;
+  if (e.field_70163_u < 9 || e.field_70163_u > 25 || e.field_70161_v > 45) return;
   evn.setCanceled(true);
+}, 'dungeon/hidewitherking').setEnabled(new StateProp(stateFloor).equals('M7').and(stateIsInBoss).and(settings._dungeonHideWitherKing));
+const partSpawnReg = reg('spawnParticle', (p, i, evn) => {
+  if (p.getY() >= 9 && p.getY() <= 25 && p.getZ() <= 45 && i.toString() === 'SPELL_WITCH') cancel(evn);
 }, 'dungeon/hidewitherking').setEnabled(new StateProp(stateFloor).equals('M7').and(stateIsInBoss).and(settings._dungeonHideWitherKing));
 
 export function init() { }
 export function start() {
   entSpawnReg.register();
+  partSpawnReg.register();
 }
 export function reset() {
   entSpawnReg.unregister();
+  partSpawnReg.unregister();
 }
