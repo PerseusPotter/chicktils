4ac98ec06208c38450c7fba62c0f69097652cc54
Ch1ck3nNeedsRNG <chick_is_bored>
2024-06-30 19:47:17 -0400

    run in main thread

--- util/threading.js
+++ util/threading.js
@@ -1,4 +1,10 @@
 const Threading = Java.type('gg.essential.api.utils.Multithreading');
+const mainThread = Thread.currentThread();
 export function run(cb) {
-  Threading.runAsync(cb);
+  if (mainThread === Thread.currentThread()) Threading.runAsync(cb);
+  else cb();
+}
+export function unrun(cb) {
+  if (mainThread === Thread.currentThread()) cb();
+  else Client.scheduleTask(cb);
 }
