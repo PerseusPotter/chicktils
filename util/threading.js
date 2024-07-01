812851c9c8e9cc4d09d904477a82a981de383799
Ch1ck3nNeedsRNG <chick_is_bored>
2024-06-30 23:26:02 -0400

    main thread fix

--- util/threading.js
+++ util/threading.js
@@ -1,7 +1,8 @@
 const Threading = Java.type('gg.essential.api.utils.Multithreading');
-const mainThread = Thread.currentThread();
+let mainThread;
+Client.scheduleTask(() => mainThread = Thread.currentThread());
 export function run(cb) {
-  if (mainThread === Thread.currentThread()) Threading.runAsync(cb);
+  if (!mainThread || mainThread === Thread.currentThread()) Threading.runAsync(cb);
   else cb();
 }
 export function unrun(cb) {
