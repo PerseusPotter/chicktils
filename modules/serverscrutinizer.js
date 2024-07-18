0d50382e60a7b456c71c0bc8b24c3daf34466d86
Ch1ck3nNeedsRNG <chick_is_bored>
2024-07-17 21:18:37 -0400

    optimize calc tps things

--- modules/serverscrutinizer.js
+++ modules/serverscrutinizer.js
@@ -5,67 +5,112 @@ import { colorForNumber } from '../util/format';
 import { log } from '../util/log';
 import reg from '../util/registerer';
 
-let ticks = [];
-let lastLoadTime = Date.now();
-let lastTickTime = Date.now();
-const cap = n => settings.serverScrutinizerTPSDisplayCap20 ? Math.min(20, n) : n;
-function trimTicks() {
-  const t = Date.now();
-  // sometimes undefined is added to array???
-  while (ticks.length > 0 && (t - ticks[ticks.length - 1] > settings.serverScrutinizerTPSMaxAge || !ticks[ticks.length - 1])) ticks.pop();
-}
-let lTps = 0;
-let rTps = 0;
-let cTps = 0;
-let aTps = 0;
-let mTps = 0;
-function calcTps() {
-  if (ticks.length === 0) {
-    cTps = 0;
-    aTps = 0;
-    mTps = 0;
-    return;
+class TickInfo {
+  arr = [];
+  min = [];
+  // rhino is bad with sparse arrays
+  idx = [];
+  maxAge = 0;
+  maxSpan = 0;
+  dirty = true;
+  cspan = 0;
+  avg = 0;
+  mspan = 0;
+  cap = Number.POSITIVE_INFINITY;
+  /**
+   * @param {number} maxAge
+   * @param {number} maxSpan
+   * @param {number?} cap
+   */
+  constructor(maxAge, maxSpan, cap) {
+    this.maxAge = maxAge;
+    this.maxSpan = maxSpan;
+    if (cap) this.cap = cap;
   }
-  if (ticks[0] === lTps && ticks[ticks.length - 1] === rTps) return;
-  lTps = ticks[0];
-  rTps = ticks[ticks.length - 1];
-  const d = Date.now();
+  _mark() {
+    this.dirty = true;
+    return this;
+  }
+  add(val) {
+    if (!val) return;
+    this.arr.unshift(val);
+    this.cspan++;
+    this._mark();
+  }
+  clear() {
+    this.arr = [];
+    this._mark();
+  }
+  _calc() {
+    if (!this.dirty) return;
+    this.dirty = false;
+    if (this.arr.length === 0) return void (this.cspan = this.avg = this.mspan = 0);
 
-  cTps = 0;
-  while (ticks.length > cTps && d - ticks[cTps] <= 1_000) cTps++;
+    const d = Date.now();
+    // this.cspan = 0;
+    // while (this.arr.length > this.cspan && d - this.arr[this.cspan] <= this.maxSpan) this.cspan++;
+    // while the cspan can only increase by 1, it could jump if ._calc() is not called after every .add(), so we instead increase the current span in add
+    // this.cspan++;
+    // in the unlikely case of a .clear() or spike followed by a .add() before a .calc()
+    this.cspan = Math.min(this.cspan, this.arr.length - 1);
+    while (this.cspan > 0 && d - this.arr[this.cspan - 1] > this.maxSpan) this.cspan--;
 
-  aTps = ticks.length / settings.serverScrutinizerTPSMaxAge * 1000;
+    this.avg = this.arr.length / this.maxAge * this.maxSpan;
 
-  if (ticks.length === 1) mTps = 1;
-  else {
-    let lt = d;
-    let l = 0, r = -1;
-    mTps = Number.POSITIVE_INFINITY;
-    while (l < ticks.length && r < ticks.length - 1) {
-      while (r < ticks.length - 1 && lt - ticks[r + 1] <= 1_000) r++;
-      let d = r - l + 1;
-      if (
-        r === l &&
-        (
-          r < ticks.length - 1 && ticks[r] - ticks[r + 1] > 1_000 ||
-          r > 0 && ticks[r - 1] - ticks[r] > 1_000 ||
-          r === 0 && d - ticks[r] > 1_000
-        )
-      ) d = 0;
-      mTps = Math.min(mTps, d);
-      l++;
-      lt = ticks[l];
+    const i = (function(arr, v) {
+      let l = 0;
+      let r = arr.length;
+      while (l !== r) {
+        let m = (l + r) >> 1;
+        if (arr[m] === v) return m;
+        if (arr[m] < v) l = m + 1;
+        else r = m;
+      }
+      return r;
+    }(this.idx, this.cspan));
+    this.idx = this.idx.slice(0, i);
+    this.idx.push(this.cspan);
+    this.min[this.cspan] = this.arr[0];
+
+    while (true) {
+      let i = this.idx[0];
+      if (d - this.min[i] > this.maxAge) this.idx.shift();
+      else {
+        this.mspan = i;
+        break;
+      }
     }
   }
+  trim() {
+    const t = Date.now();
+    while (this.arr.length > 0 && t - this.arr[this.arr.length - 1] > this.maxAge) this._mark().arr.pop();
+  }
+  getCur() {
+    this._calc();
+    return Math.min(this.cap, this.cspan);
+  }
+  getAvg() {
+    this._calc();
+    return Math.min(this.cap, this.avg);
+  }
+  getMin() {
+    this._calc();
+    return Math.min(this.cap, this.mspan);
+  }
 }
+
+let lastLoadTime = Date.now();
+let lastTickTime = Date.now();
+const ticks = new TickInfo(settings.serverScrutinizerTPSMaxAge, 1_000, settings.serverScrutinizerTPSDisplayCap20 ? 20 : undefined);
 
 const serverTickReg = reg('packetReceived', () => {
   const t = Date.now();
-  ticks.unshift(t);
+  ticks.add(t);
   lastTickTime = t;
+  lastLoadTime = 0;
 }, 'serverscrutinizer').setFilteredClass(Java.type('net.minecraft.network.play.server.S32PacketConfirmTransaction'));
 const worldLoadReg = reg('worldLoad', () => {
-  ticks = [];
+  ticks.clear();
   lastLoadTime = lastTickTime = Date.now();
 }, 'serverscrutinizer');
 
@@ -74,11 +119,10 @@ function getTPSColor(tps) {
 }
 
 const tpsCmd = reg('command', () => {
-  trimTicks();
-  calcTps();
-  log('Current TPS:', getTPSColor(cTps) + cTps);
-  log('Average TPS:', getTPSColor(aTps) + aTps.toFixed(1));
-  log('Minimum TPS:', getTPSColor(mTps) + mTps);
+  ticks.trim();
+  log('Current TPS:', getTPSColor(ticks.getCur()) + ticks.getCur());
+  log('Average TPS:', getTPSColor(ticks.getAvg()) + ticks.getAvg().toFixed(1));
+  log('Minimum TPS:', getTPSColor(ticks.getMin()) + ticks.getMin());
 }, 'serverscrutinizer').setName('tps');
 
 function formatTps(curr, avg, min) {
@@ -95,9 +139,8 @@ function formatTps(curr, avg, min) {
 }
 const tpsDisplay = createTextGui(() => data.serverScrutinizerTPSDisplay, () => formatTps(20, 18.4, 11));
 const rendOvTps = reg('renderOverlay', () => {
-  trimTicks();
-  calcTps();
-  tpsDisplay.setLines(formatTps(cap(cTps), cap(aTps), cap(mTps)));
+  ticks.trim();
+  tpsDisplay.setLines(formatTps(ticks.getCur(), ticks.getAvg(), ticks.getMin()));
   tpsDisplay.render();
 }, 'serverscrutinizer').setEnabled(settings._serverScrutinizerTPSDisplay);
 
@@ -105,8 +148,7 @@ const lastTickDisplay = createTextGui(() => data.serverScrutinizerLastPacketDisp
 const rendOvLTD = reg('renderOverlay', () => {
   const d = Date.now();
   if (d - lastLoadTime < 11_000) return;
-  trimTicks();
-  const t = d - (ticks.length ? ticks[0] : lastTickTime);
+  const t = d - lastTickTime;
   if (t < settings.serverScrutinizerLastTickThreshold) return;
   lastTickDisplay.setLine(`zzz for ${colorForNumber(2000 - t, 2000)}${(t / 1000).toFixed(2)}s`);
   lastTickDisplay.render();
@@ -115,9 +157,11 @@ const rendOvLTD = reg('renderOverlay', () => {
 export function init() {
   settings._moveTPSDisplay.onAction(() => tpsDisplay.edit());
   settings._moveLastTickDisplay.onAction(() => lastTickDisplay.edit());
+  settings._serverScrutinizerTPSMaxAge.onAfterChange(v => ticks.maxAge = v);
+  settings._serverScrutinizerTPSDisplayCap20.onAfterChange(v => ticks.cap = v ? 20 : Number.POSITIVE_INFINITY);
 }
 export function load() {
-  ticks = [];
+  ticks.clear();
 
   serverTickReg.register();
   worldLoadReg.register();
