001a5fa8314876dbd84f6556a5648028be055e96
Ch1ck3nNeedsRNG <chick_is_bored>
2024-07-18 02:22:17 -0400

    v0.6.3

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { calcMedian } from './util/math';
 import { _setTimeout } from './util/timers';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.6.2';
+const VERSION = '0.6.3';
 
 let sev;
 function tryUpdate(delay = 0) {
