23712540cab021d18ff668b98f134f8fecbf4c0d
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-07-31 05:18:50 -0400

    error checking

--- util/threading.js
+++ util/threading.js
@@ -1,11 +1,24 @@
+import { log } from './log';
+
 const Threading = Java.type('gg.essential.api.utils.Multithreading');
 let mainThread;
 Client.scheduleTask(() => mainThread = Thread.currentThread());
 export function run(cb) {
-  if (!mainThread || mainThread === Thread.currentThread()) Threading.runAsync(cb);
-  else cb();
+  if (!mainThread || mainThread === Thread.currentThread()) Threading.runAsync(wrap(cb));
+  else wrap(cb)();
 }
 export function unrun(cb) {
   if (mainThread === Thread.currentThread()) cb();
   else Client.scheduleTask(cb);
+}
+export function wrap(cb) {
+  return function() {
+    try {
+      cb();
+    } catch (e) {
+      log('error', e);
+      log(e.stack);
+      console.error(e);
+    }
+  };
 }
