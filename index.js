2e55f11e7ef452e13f92c235cfad270834b05e7a
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-08-11 03:37:49 -0400

    v0.6.15

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ import { calcMedian } from './util/math';
 import { _setTimeout } from './util/timers';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.6.14';
+const VERSION = '0.6.15';
 
 let sev;
 function tryUpdate(delay = 0) {
