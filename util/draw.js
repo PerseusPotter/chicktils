5b473fa7c48ae9961021d2a81768fde117a4845c
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-09-21 12:35:19 -0400

    whoops

--- util/draw.js
+++ util/draw.js
@@ -38,7 +38,7 @@ function getPitch() {
 let lastServerTickTime = Date.now();
 let cachedServerTickPartial = 0;
 reg('serverTick', () => lastServerTickTime = Date.now(), 'draw');
-reg(net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent, () => cachedServerTickPartial = Math.min(1, (Date.now() - lastServerTickTime) / 50));
+reg(net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent, () => cachedServerTickPartial = Math.min(1, (Date.now() - lastServerTickTime) / 50), 'draw');
 export function getPartialServerTick() {
   return cachedServerTickPartial;
 }
