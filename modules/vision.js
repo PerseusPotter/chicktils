3bfc58ec08f95b6ca0f6f1c3f656b17e0bbdd5a0
Ch1ck3nNeedsRNG <chick_is_bored>
2024-09-21 18:05:24 -0400

    vision

---
+++ modules/vision.js
@@ -0,0 +1,15 @@
+import reg from '../util/registerer';
+
+const potionReg = reg('packetReceived', (pack, evn) => {
+  if (pack.func_149426_d() !== Player.getPlayer().func_145782_y()) return;
+  if (pack.func_149427_e() !== 15) return;
+  cancel(evn);
+}, 'vision').setFilteredClass(net.minecraft.network.play.server.S1DPacketEntityEffect);
+
+export function init() { }
+export function load() {
+  potionReg.register();
+}
+export function unload() {
+  potionReg.unregister();
+}
