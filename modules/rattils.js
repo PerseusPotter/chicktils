3f029e01cff8c370f855f6f39eb0420a28b7c7a5
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-02 01:10:49 -0400

    fix ids

--- modules/rattils.js
+++ modules/rattils.js
@@ -24,25 +24,10 @@ const EntityItem = Java.type('net.minecraft.entity.item.EntityItem');
 function scanCheese() {
   run(() => {
     cheese = World.getAllEntitiesOfType(EntityItem).filter(v => {
-      /*
-      {
-        SkullOwner: {
-          Id: "d91490fc-b575-3128-a4e3-0cc0f9f1d23c",
-          Properties: {
-            textures: [
-              0: {
-                Value: "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzE1MzlkYmNkMzZmODc3MjYzMmU1NzM5ZTJlNTE0ODRlZGYzNzNjNTU4ZDZmYjJjNmI2MWI3MmI3Y2FhIn19fQ"
-              }
-            ]
-          }
-        }
-      }
-      */
       const nbt = v.entity.func_92059_d().func_77978_p();
       if (!nbt) return false;
       const id = nbt.func_74775_l('SkullOwner').func_74779_i('Id');
-      // const texture = nbt.func_74775_l('SkullOwner').func_74775_l('Properties').func_150295_c('textures', 10).func_150305_b(0).func_74779_i('Value');
-      return id === 'd91490fc-b575-3128-a4e3-0cc0f9f1d23c'; // && texture === 'eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzE1MzlkYmNkMzZmODc3MjYzMmU1NzM5ZTJlNTE0ODRlZGYzNzNjNTU4ZDZmYjJjNmI2MWI3MmI3Y2FhIn19fQ';
+      return id === '9675d9a3-d888-3c30-93b1-8cac5b9be1f4';
     });
   });
 }
