956ba9f5da570a21760ee0ccf2d303fe484b62d1
Ch1ck3nNeedsRNG <chick_is_bored>
2024-11-03 00:11:40 -0400

    change reg & remove box self

--- modules/dungeon/boxteammates.js
+++ modules/dungeon/boxteammates.js
@@ -3,27 +3,19 @@ import { renderOutline } from '../../util/draw';
 import reg from '../../util/registerer';
 import { getPlayers, registerTrackPlayers } from '../dungeon.js';
 
-const renderWorldReg = reg('renderWorld', () => {
-  getPlayers().forEach(v => {
-    if (!v.e) return;
-    if (v.e === Player) {
-      if (!settings.dungeonBoxTeammatesBoxSelf) return;
-      if (Client.settings.getSettings().field_74320_O === 0) return;
-    } else if (v.e.isDead()) return;
-    const x = v.e.getRenderX();
-    const y = v.e.getRenderY();
-    const z = v.e.getRenderZ();
-    const c = settings[`dungeonBoxTeammates${v.class.slice(0, 4)}Color`] ?? settings.boxAllEntitiesColor;
-    renderOutline(x, y, z, 0.8, 2, c, settings.dungeonBoxTeammatesEsp);
-  });
-}).setEnabled(settings._dungeonBoxTeammates);
+const postRenderReg = reg('postRenderEntity', (ent, pos) => {
+  const p = getPlayers().find(v => v.me === ent.entity);
+  if (!p) return;
+  const c = settings[`dungeonBoxTeammates${p.class.slice(0, 4)}Color`] ?? settings.boxAllEntitiesColor;
+  renderOutline(pos.getX(), pos.getY(), pos.getZ(), 0.8, 2, c, settings.dungeonBoxTeammatesEsp, true, undefined, true);
+}).setFilteredClass(net.minecraft.client.entity.EntityOtherPlayerMP).setEnabled(settings._dungeonBoxTeammates);
 
 export function init() {
   registerTrackPlayers(settings._dungeonBoxTeammates);
 }
 export function start() {
-  renderWorldReg.register();
+  postRenderReg.register();
 }
 export function reset() {
-  renderWorldReg.unregister();
+  postRenderReg.unregister();
 }
