1b6e96dd0d28ce27cecb48e58eb88c019e2ed17d
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2024-11-04 16:25:09 -0500

    box teammates memoize

--- modules/dungeon/boxteammates.js
+++ modules/dungeon/boxteammates.js
@@ -3,19 +3,26 @@ import { renderOutline } from '../../util/draw';
 import reg from '../../util/registerer';
 import { getPlayers, registerTrackPlayers } from '../dungeon.js';
 
+const teammates = new (Java.type('java.util.WeakHashMap'))();
+
+const tickReg = reg('tick', () => getPlayers().forEach(v => v.me && teammates.put(v.me, v.class))).setEnabled(settings._dungeonBoxTeammates);
 const postRenderReg = reg('postRenderEntity', (ent, pos) => {
-  const p = getPlayers().find(v => v.me === ent.entity);
-  if (!p) return;
-  const c = settings[`dungeonBoxTeammates${p.class.slice(0, 4)}Color`] ?? settings.boxAllEntitiesColor;
-  renderOutline(pos.getX(), pos.getY(), pos.getZ(), 0.8, 2, c, settings.dungeonBoxTeammatesEsp, true, undefined, true);
+  const c = teammates.get(ent.entity);
+  if (!c) return;
+  const col = settings[`dungeonBoxTeammates${c.slice(0, 4)}Color`] ?? settings.boxAllEntitiesColor;
+  renderOutline(pos.getX(), pos.getY(), pos.getZ(), 0.8, 2, col, settings.dungeonBoxTeammatesEsp, true, undefined, true);
 }).setFilteredClass(net.minecraft.client.entity.EntityOtherPlayerMP).setEnabled(settings._dungeonBoxTeammates);
 
 export function init() {
   registerTrackPlayers(settings._dungeonBoxTeammates);
 }
 export function start() {
+  teammates.clear();
+
+  tickReg.register();
   postRenderReg.register();
 }
 export function reset() {
+  tickReg.unregister();
   postRenderReg.unregister();
 }
