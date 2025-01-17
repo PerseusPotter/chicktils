5a2968de8ab48187032397b306cdc264aae6ee5f
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-01-17 18:03:53 -0500

    hide coating particles

--- settings.js
+++ settings.js
@@ -1007,7 +1007,9 @@ export const props = {
   blockHighlightCantEtherWireColor: new Property('BlockHighlightCantEtherWireColor', page, ++sort, Property.Type.Color, 0xCA2207A0),
   blockHighlightCantEtherFillColor: new Property('BlockHighlightCantEtherFillColor', page, ++sort, Property.Type.Color, 0xBA2B1E60),
   blockHighlightCantEtherWireWidth: new Property('BlockHighlightCantEtherWireWidth', page, ++sort, Property.Type.Number, 3, { min: 0 }),
-  blockHighlightCantEtherShowReason: new Property('BlockHighlightCantEtherShowReason', page, ++sort, Property.Type.Toggle, true)
+  blockHighlightCantEtherShowReason: new Property('BlockHighlightCantEtherShowReason', page, ++sort, Property.Type.Toggle, true),
+
+  enablehidedivanparticles: new Property('HideDivanCoatingParticles', page, ++sort, Property.Type.Toggle, true)
 };
 const pageNames = [
   '',
