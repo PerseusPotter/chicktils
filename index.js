1b0872f3f61d2ad20f3cbcf9772dfbb66418092c
Ch1ck3nNeedsRNG <chick_is_bored>
2025-01-22 19:41:53 -0500

    v0.8.15

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import { getRegs, simulate } from './util/registerer';
 import { calcMedian } from './util/math';
 setIsMainS();
 setIsMainD();
-const VERSION = '0.8.14';
+const VERSION = '0.8.15';
 
 let sev;
 function tryUpdate(delay = 0) {
