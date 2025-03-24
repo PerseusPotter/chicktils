c15f79e8ae81a7ddaef7bb8dd5568c89484ca3e4
Ch1ck3nNeedsRNG <chick_is_bored>
2025-03-24 02:26:35 -0400

    add DSU

--- util/polyfill.js
+++ util/polyfill.js
@@ -91,3 +91,40 @@ export function shuffle(arr) {
   }
   return arr;
 }
+
+export class DSU {
+  /**
+   * @param {number} n
+   */
+  constructor(n) {
+    this.parent = new Array(n).fill(0).map((_, i) => i);
+    this.rank = new Array(n).fill(0);
+  }
+
+  /**
+   * @param {number} x
+   * @returns {number}
+   */
+  find(x) {
+    if (this.parent[x] !== x) this.parent[x] = this.find(this.parent[x]);
+    return this.parent[x];
+  }
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   */
+  union(x, y) {
+    let rootX = this.find(x);
+    let rootY = this.find(y);
+
+    if (rootX !== rootY) {
+      if (this.rank[rootX] > this.rank[rootY]) this.parent[rootY] = rootX;
+      else if (this.rank[rootX] < this.rank[rootY]) this.parent[rootX] = rootY;
+      else {
+        this.parent[rootY] = rootX;
+        this.rank[rootX]++;
+      }
+    }
+  }
+}
