7fadb2625735d0ae28690ff97d1e1a93fef50b00
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-03-28 19:53:25 -0400

    add getBlockId

--- modules/dungeon/simonsays.js
+++ modules/dungeon/simonsays.js
@@ -2,7 +2,7 @@ import settings from '../../settings';
 import reg from '../../util/registerer';
 import { compareFloat } from '../../util/math';
 import { StateProp, StateVar } from '../../util/state';
-import { getBlockPos } from '../../util/mc';
+import { getBlockId, getBlockPos } from '../../util/mc';
 import { stateFloor, stateIsInBoss } from '../dungeon.js';
 import { renderAABBFilled } from '../../util/draw';
 
@@ -34,9 +34,8 @@ function onSeaLantern(x, y, z) {
   if (id) solution.push(id);
   return !!id;
 }
-const MCBlock = net.minecraft.block.Block;
 const blockChangeReg = reg('packetReceived', pack => {
-  if (MCBlock.func_149682_b(pack.func_180728_a().func_177230_c()) !== 169) return;
+  if (getBlockId(pack.func_180728_a().func_177230_c()) !== 169) return;
 
   const pos = getBlockPos(pack.func_179827_b());
   onSeaLantern(pos.x, pos.y, pos.z);
@@ -44,7 +43,7 @@ const blockChangeReg = reg('packetReceived', pack => {
 const multiBlockReg = reg('packetReceived', pack => {
   const changes = pack.func_179844_a();
   if (changes.some(v => {
-    if (MCBlock.func_149682_b(v.func_180088_c().func_177230_c()) !== 169) return;
+    if (getBlockId(v.func_180088_c().func_177230_c()) !== 169) return;
     const pos = getBlockPos(v.func_180090_a());
     return onSeaLantern(pos.x, pos.y, pos.z);
   })) return;
@@ -53,7 +52,7 @@ const multiBlockReg = reg('packetReceived', pack => {
 
   const pos = getBlockPos(change.func_180090_a());
   if (pos.x !== 110 || pos.y !== 121 || pos.z !== 94) return;
-  if (MCBlock.func_149682_b(change.func_180088_c().func_177230_c()) !== 0) return;
+  if (getBlockId(change.func_180088_c().func_177230_c()) !== 0) return;
 
   solution = [];
 }).setFilteredClass(net.minecraft.network.play.server.S22PacketMultiBlockChange).setEnabled(stateDoSimonSays);
