361e95d92862ff7eb03742de258b783198f616e5
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-04-12 10:36:43 -0400

    wrap

--- util/threading.js
+++ util/threading.js
@@ -4,10 +4,12 @@ const Threading = Java.type('gg.essential.api.utils.Multithreading');
 let mainThread;
 Client.scheduleTask(() => mainThread = Thread.currentThread());
 export function run(cb) {
-  if (!mainThread || mainThread === Thread.currentThread()) Threading.runAsync(wrap(cb));
-  else wrap(cb)();
+  cb = wrap(cb)
+  if (!mainThread || mainThread === Thread.currentThread()) Threading.runAsync(cb);
+  else cb();
 }
 export function unrun(cb) {
+  cb = wrap(cb)
   if (mainThread === Thread.currentThread()) cb();
   else Client.scheduleTask(cb);
 }
