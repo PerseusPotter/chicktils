fc89c1d0f0e1bad92fed89705752a40239ef46f2
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-09-16 22:46:35 -0400

    update if party says

--- modules/dungeon/princekilled.js
+++ modules/dungeon/princekilled.js
@@ -3,14 +3,20 @@ import reg from '../../util/registerer';
 import { StateProp } from '../../util/state';
 import { statePrinceKilled } from '../dungeon.js';
 
+const stateNoPrince = new StateProp(statePrinceKilled).not();
 const chatReg = reg('chat', () => {
   if (settings.dungeonPrinceKilledMessage) ChatLib.command('pc Prince Killed!');
   statePrinceKilled.set(true);
-}).setCriteria('&r&eA Prince falls. &r&a+1 Bonus Score&r').setEnabled(new StateProp(statePrinceKilled).not());
+}).setCriteria('&r&eA Prince falls. &r&a+1 Bonus Score&r').setEnabled(stateNoPrince);
+const partyReg = reg('chat', (ign, msg) => {
+  statePrinceKilled.set(true);
+}).setCriteria('&r&9Party &8> ${ign}&f: &r${msg}&r').setEnabled(stateNoPrince);
 
 export function start() {
   chatReg.register();
+  partyReg.register();
 }
 export function reset() {
   chatReg.unregister();
+  partyReg.unregister();
 }
