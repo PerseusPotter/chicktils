a81b9b83c23ed15ed42a3cf22d1bcd00cb0a3def
Ch1ck3nNeedsRNG <chick_is_bored>
2025-09-16 22:46:28 -0400

    remove paul check

--- modules/dungeon/guessmimic.js
+++ modules/dungeon/guessmimic.js
@@ -6,7 +6,7 @@ import { StateProp } from '../../util/state';
 import { stateIsInBoss, statePrinceKilled } from '../dungeon.js';
 
 const ScoreCalculation = JavaTypeOrNull('gg.skytils.skytilsmod.features.impl.dungeons.ScoreCalculation')?.INSTANCE;
-const notifs = [false, false, false, false, false];
+const notifs = [false, false, false];
 
 const scoreUpdateReg = reg('packetReceived', pack => {
   if (pack.func_149307_h() !== 2) return;
@@ -24,33 +24,21 @@ const scoreUpdateReg = reg('packetReceived', pack => {
     const stScore = ScoreCalculation.getTotalScore().get() + (statePrinceKilled.get() ? 1 : 0);
 
     let diff = score - stScore;
-    if (diff > 4 && !ScoreCalculation.isPaul().get()) {
-      if (!notifs[0]) log(`&dGuessing that Paul is active (score difference of ${diff})`);
-      notifs[0] = true;
-      ScoreCalculation.isPaul().set(true);
-      diff -= 10;
-    }
-    if (diff < 5 && ScoreCalculation.isPaul().get()) {
-      if (!notifs[1]) log(`&dGuessing that Paul is no longer active (score difference of ${diff})`);
-      notifs[1] = true;
-      ScoreCalculation.isPaul().set(false);
-      diff += 10;
-    }
     if (diff > 0 && ScoreCalculation.getDeaths().get() > 0 && !ScoreCalculation.getFirstDeathHadSpirit().get()) {
-      if (!notifs[2]) log(`&dGuessing that first death DID have spirit (score difference of ${diff})`);
-      notifs[2] = true;
+      if (!notifs[0]) log(`&dGuessing that first death DID have spirit (score difference of ${diff})`);
+      notifs[0] = true;
       ScoreCalculation.getFirstDeathHadSpirit().set(true);
       diff--;
     }
     if (diff >= 2 && !ScoreCalculation.getMimicKilled().get()) {
-      if (!notifs[3]) log(`&dGuessing that mimic has been killed (score difference of ${diff})`);
-      notifs[3] = true;
+      if (!notifs[1]) log(`&dGuessing that mimic has been killed (score difference of ${diff})`);
+      notifs[1] = true;
       ScoreCalculation.getMimicKilled().set(true);
       diff -= 2;
     }
     if (diff >= 1 && !statePrinceKilled.get()) {
-      if (!notifs[4]) log(`&dGuessing that a prince has been killed (score difference of ${diff})`);
-      notifs[4] = true;
+      if (!notifs[2]) log(`&dGuessing that a prince has been killed (score difference of ${diff})`);
+      notifs[2] = true;
       statePrinceKilled.set(true);
       diff--;
     }
