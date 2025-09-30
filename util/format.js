0f9794e039d4916c274327135c1de49b264069ac
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-09-30 04:09:19 -0400

    rename format -> serialize, move to format, and add additional objects

--- util/format.js
+++ util/format.js
@@ -1,5 +1,7 @@
-import { log } from './log';
+import { base64Encode, base64EncodeInt } from './helper';
+import { getItemIdI } from './mc';
 import { JavaTypeOrNull } from './polyfill';
+import { getSbId } from './skyblock';
 
 /**
  * @param {number} t
@@ -100,3 +102,126 @@ export function getPlayerName(name) {
 export function formatQuantity(amnt, name = '') {
   return `${amnt > 0 ? '&a+' : amnt < 0 ? '&c-' : '&e±'} ${commaNumber(Math.abs(amnt))}x${name ? ' &r' + name : ''}`;
 }
+
+export const serialize = (function() {
+  const JavaNumber = Java.type('java.lang.Number');
+  const CTNativeJavaList = Java.type('org.mozilla.javascript.NativeJavaList');
+  const JavaMap = Java.type('java.util.Map');
+
+  const Vector3f = Java.type('org.lwjgl.util.vector.Vector3f');
+  const UUID = Java.type('java.util.UUID');
+
+  const MCVec3 = Java.type('net.minecraft.util.Vec3');
+  const MCVec3i = Java.type('net.minecraft.util.Vec3i');
+  const MCVec4b = Java.type('net.minecraft.util.Vec4b');
+  const MCRotations = Java.type('net.minecraft.util.Rotations');
+
+  const MCBlock = Java.type('net.minecraft.block.Block');
+  const MCIBlockState = Java.type('net.minecraft.block.state.IBlockState');
+  const MCItem = Java.type('net.minecraft.item.Item');
+  const MCItemStack = Java.type('net.minecraft.item.ItemStack');
+  const MCIChatComponent = Java.type('net.minecraft.util.IChatComponent');
+
+  const MCNBTBase = Java.type('net.minecraft.nbt.NBTBase');
+  const MCNBTTagByte = Java.type('net.minecraft.nbt.NBTTagByte');
+  const MCNBTTagDouble = Java.type('net.minecraft.nbt.NBTTagDouble');
+  const MCNBTTagFloat = Java.type('net.minecraft.nbt.NBTTagFloat');
+  const MCNBTTagInt = Java.type('net.minecraft.nbt.NBTTagInt');
+  const MCNBTTagLong = Java.type('net.minecraft.nbt.NBTTagLong');
+  const MCNBTTagShort = Java.type('net.minecraft.nbt.NBTTagShort');
+  const MCNBTTagString = Java.type('net.minecraft.nbt.NBTTagString');
+  const MCNBTTagByteArray = Java.type('net.minecraft.nbt.NBTTagByteArray');
+  const MCNBTTagIntArray = Java.type('net.minecraft.nbt.NBTTagIntArray');
+  const MCNBTTagCompound = Java.type('net.minecraft.nbt.NBTTagCompound');
+  const MCNBTTagList = Java.type('net.minecraft.nbt.NBTTagList');
+  const MCNBTTagEnd = Java.type('net.minecraft.nbt.NBTTagEnd');
+  const MCNBTTagByteArray$getData = MCNBTTagByteArray.class.getDeclaredMethod('func_150292_c');
+  const MCNBTTagIntArray$getData = MCNBTTagIntArray.class.getDeclaredMethod('func_150302_c');
+
+  /**
+   * @param {any} obj
+   * @param {number} [depth = 3]
+   * @param {object} [options = {}]
+   * @param {number} [options.precision = 3]
+   * @returns {string}
+   */
+  return function(obj, depth = 3, options = {}) {
+    let t = 'object';
+    const { precision = 3 } = options;
+    try {
+      t = typeof obj;
+    } catch (e) { }
+    switch (t) {
+      case 'string': return `'${obj}'`;
+      case 'number': return Number.isInteger(obj) || precision < 0 ? obj.toString() : obj.toFixed(precision);
+      case 'bigint': return obj.toString() + 'n';
+      case 'boolean': return obj.toString();
+      case 'function': return `function ${'name' in obj ? obj.name : obj.class.getSimpleName()}() {}`
+      case 'symbol': return '@@' + obj.description;
+      case 'undefined': return 'undefined';
+      case 'object':
+        if (obj === null) return 'null';
+        if (obj instanceof Date) return obj.toISOString();
+        if (obj instanceof Error) return obj.toString();
+        if (obj instanceof Set) return 'Set' + serialize(Array.from(obj.keys()), depth, options);
+        if (obj instanceof Map) return 'Map' + serialize(Array.from(obj.entries()).reduce((a, [k, v]) => (a[k] = v, a), {}), depth, options);
+        if (Array.isArray(obj)) {
+          if (depth === 0) return `Array(${obj.length})`;
+          return `[${obj.map(v => serialize(v, depth - 1, options)).join(', ')}]`;
+        }
+
+        if (obj.getClass?.()?.isEnum?.()) return `{Enum|${obj.name()}}`;
+        if (obj instanceof JavaNumber) return serialize(obj.doubleValue(), depth, options);
+
+        if (obj instanceof CTNativeJavaList) return 'JavaArray' + serialize(Array.from(obj), depth, options);
+        if (obj instanceof JavaMap) return 'JavaMap' + serialize(obj.entrySet().reduce((a, v) => (a[v.getKey()] = v.getValue(), a), {}));
+
+        if (obj instanceof Vector3f) return `Vector3f<${serialize(obj.x, depth, options)}, ${serialize(obj.y, depth, options)}, ${serialize(obj.z, depth, options)}>`;
+        if (obj instanceof UUID) return `{UUID|${serialize(obj.toString(), depth, options)}}`;
+
+        if (obj instanceof MCVec3) return `Vec3<${serialize(obj.field_72450_a, depth, options)}, ${serialize(obj.field_72448_b, depth, options)}, ${serialize(obj.field_72449_c, depth, options)}>`;
+        if (obj instanceof MCVec3i) return `Vec3i(${serialize(obj.func_177958_n(), depth, options)}, ${serialize(obj.func_177956_o(), depth, options)}, ${serialize(obj.func_177952_p(), depth, options)})`;
+        if (obj instanceof MCVec4b) return `Vec4b(${serialize(obj.func_176110_a(), depth, options)}, ${serialize(obj.func_176112_b(), depth, options)}, ${serialize(obj.func_176113_c(), depth, options)}, ${serialize(obj.func_176111_d(), depth, options)})`;
+        if (obj instanceof MCRotations) return `Rotations[${serialize(obj.func_179415_b(), depth, options)}i + ${serialize(obj.func_179416_c(), depth, options)}j + ${serialize(obj.func_179413_d(), depth, options)}k]`;
+
+        if (obj instanceof MCBlock) return `{Block|${serialize(obj.func_149732_F(), depth, options)}|${serialize(MCBlock.func_149682_b(obj), depth, options)}}`;
+        if (obj instanceof MCIBlockState) return `{IBlockState|${serialize(obj.func_177230_c(), depth, options)}|meta=${serialize(obj.func_177230_c().func_176201_c(obj), depth, options)}}`;
+        if (obj instanceof MCItem) return `{Item|${serialize(getItemIdI(obj), depth, options)}}`;
+        if (obj instanceof MCItemStack) return `{ItemStack|${serialize(obj.func_77973_b(), depth, options)}|damage=${serialize(obj.func_77952_i(), depth, options)}|count=${serialize(obj.field_77994_a, depth, options)}|sbid=${serialize(getSbId(obj), depth, options)}}`;
+        if (obj instanceof MCIChatComponent) return `{IChatComponent|${serialize(obj.func_150254_d(), depth, options)}}`;
+
+        if (obj instanceof MCNBTBase) {
+          if (obj instanceof MCNBTTagByte) return `{NBTTagByte|${serialize(obj.func_150290_f(), depth, options)}}`;
+          if (obj instanceof MCNBTTagDouble) return `{NBTTagDouble|${serialize(obj.func_150286_g(), depth, options)}}`;
+          if (obj instanceof MCNBTTagFloat) return `{NBTTagFloat|${serialize(obj.func_150288_h(), depth, options)}}`;
+          if (obj instanceof MCNBTTagInt) return `{NBTTagInt|${serialize(obj.func_150287_d(), depth, options)}}`;
+          if (obj instanceof MCNBTTagLong) return `{NBTTagLong|${serialize(obj.func_150291_c(), depth, options)}}`;
+          if (obj instanceof MCNBTTagShort) return `{NBTTagShort|${serialize(obj.func_150289_e(), depth, options)}}`;
+          if (obj instanceof MCNBTTagString) return `{NBTTagString|${serialize(obj.func_150285_a_(), depth, options)}}`;
+          if (obj instanceof MCNBTTagByteArray) return `{NBTTagByteArray|${serialize(base64Encode(MCNBTTagByteArray$getData, obj, []), depth, options)}}`;
+          if (obj instanceof MCNBTTagIntArray) return `{NBTTagByteArray|${serialize(base64EncodeInt(MCNBTTagIntArray$getData, obj, []), depth, options)}}`;
+          if (obj instanceof MCNBTTagCompound) {
+            const keys = obj.func_150296_c();
+            const newObj = {};
+            keys.forEach(v => newObj[v] = obj.func_74781_a(v));
+            return `{NBTTagCompound|${serialize(newObj, depth, options)}}`;
+          }
+          if (obj instanceof MCNBTTagList) {
+            const l = obj.func_74745_c();
+            const arr = [];
+            for (let i = 0; i < l; i++) {
+              arr.push(obj.func_179238_g(i));
+            }
+            return `{NBTTagList|${serialize(arr, depth, options)}}`;
+          }
+          if (obj instanceof MCNBTTagEnd) return `{NBTTagEnd}`;
+          return `{${obj.getClass().getSimpleName()}}`;
+        }
+
+        if (depth === 0) return `[object ${obj.constructor ? obj.constructor.name : 'Object'}]`;
+        const ent = Object.entries(obj);
+        if (ent.length === 0) return '{}';
+        return '{ ' + ent.map(([k, v]) => `${k}: ${serialize(v, depth - 1, options)}`).join(', ') + ' }';
+    }
+  };
+}());
