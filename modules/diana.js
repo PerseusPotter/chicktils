35f3094a788edf3a3d018882f7e630d078987ec6
chicken man <60198737+PerseusPotter@users.noreply.github.com>
2025-11-08 14:06:24 -0500

    diana changes (no longer uses sound)

--- modules/diana.js
+++ modules/diana.js
@@ -3,13 +3,14 @@ import data from '../data';
 import reg, { customRegs, execCmd } from '../util/registerer';
 import { log } from '../util/log';
 import createAlert from '../util/alert';
-import { compareFloat, convergeHalfInterval, dist, geoMedian, gradientDescent, linReg, lineRectColl, ndRegression, newtonRaphson, rescale, toPolynomial } from '../util/math';
-import { getBlockPos, getItemId, getLowerContainer } from '../util/mc';
+import { binomial, compareFloat, convergeHalfInterval, geoMedian, lineRectColl, ndRegression, pearsonCoeff, rescale, toPolynomial } from '../util/math';
+import { getBlockPos, getItemId, getLastReportedX, getLastReportedY, getLastReportedZ, getLowerContainer } from '../util/mc';
 import { unrun } from '../util/threading';
 import { renderBeacon, renderBillboardString, renderBoxOutline, renderLine } from '../../Apelles/index';
-import { toArrayList } from '../util/polyfill';
+import { Deque, shuffle, toArrayList } from '../util/polyfill';
 import { getSbId } from '../util/skyblock';
 import createPointer from '../util/pointto';
+import { AtomicStateVar } from '../util/state';
 
 const warps = [
   {
@@ -104,9 +105,8 @@ const burrowSpawnReg = reg('packetReceived', pack => {
   const z = Math.floor(pack.func_149225_f()) + 0.5;
 
   unrun(() => {
-    if (guessPos.has('Average')) {
-      const p = guessPos.get('Average');
-      const d = (x - p[0]) ** 2 + (z - p[2]) ** 2;
+    if (guessPos) {
+      const d = (x - guessPos[0]) ** 2 + (z - guessPos[2]) ** 2;
       if (d < 400) foundGuessBurrow = true;
     }
 
@@ -129,7 +129,7 @@ const burrowDigReg = reg('packetSent', pack => {
   if (!bp) return;
 
   const { x, y, z } = getBlockPos(bp);
-  if (x === -1 && y === -1 && z === -1) return;
+  if (x === -1 && y === -1 && z === -1) return
 
   unrun(() => {
     const burrowI = burrows.findIndex(v => v[0] === x + 0.5 && v[1] === y && v[2] === z + 0.5);
@@ -178,28 +178,30 @@ const renderTargetsReg = reg('renderWorld', () => {
   );
 });
 
-let spadeUseTime = 0;
-/** @type {{ t: number, p: number }[]} */
-let prevSounds = [];
-/** @type {{ t: number, x: number, y: number, z: number }[]} */
-let prevParticles = [];
-/** @type {Map<'Average' | 'Spline' | 'MLAT' | 'Bezier', [number, number, number]?>} */
-let guessPos = new Map();
-/** @type {[(t: number) => number, (t: number) => number, (t: number) => number]?} */
-let splinePoly;
-let splinePolyPos = toArrayList([]);
-let prevGuessL = 0;
-const RESET_THRESH = 20;
-function resetGuess() {
-  spadeUseTime = getTickCount();
-  prevSounds = [];
-  prevParticles = [];
+/** @typedef {{ t: number, x: number, y: number, z: number }} ParticlePos */
+/** @type {Deque<[number, number, number]>} */
+const spadeUsePositions = new Deque();
+/** @type {Deque<ParticlePos>} */
+const unclaimedParticles = new Deque();
+/** @type {Deque<ParticlePos>} */
+const possibleStartingParticles = new Deque();
+/** @type {ParticlePos[]} */
+let knownParticleChain = [];
+const MIN_CHAIN_LENGTH = 4;
+const MIN_CHAIN_PEARSON_SQ = 0.7;
+const RANSAC_ITERS_PER = 5;
 
-  prevGuessL = 0;
-  splinePoly = null;
+/** @type {[number, number, number]?} */
+let guessPos = null;
+/** @type {import('../util/state').StateVar<[(t: number) => number, (t: number) => number, (t: number) => number]?>} */
+const splinePoly = new AtomicStateVar(null);
+let splinePolyPos = toArrayList([]);
+function resetGuess() {
+  splinePoly.set(null);
+  knownParticleChain = [];
   unrun(() => {
-    if (settings.dianaGuessRememberPrevious && !foundGuessBurrow && guessPos.has('Average')) {
-      const v = guessPos.get('Average');
+    if (settings.dianaGuessRememberPrevious && !foundGuessBurrow && guessPos) {
+      const v = guessPos;
       if (Math.hypot(Player.getX() - v[0], Player.getY() - v[1], Player.getZ() - v[2]) > 10) {
         if (
           (
@@ -215,32 +217,28 @@ function resetGuess() {
         ) prevGuesses.push([v[0], v[1], v[2], getTickCount(), 20]);
       }
     }
-    guessPos.clear();
+    guessPos = [];
     foundGuessBurrow = false;
   });
 }
+function fullReset() {
+  resetGuess();
+  spadeUsePositions.clear();
+  unclaimedParticles.clear();
+  possibleStartingParticles.clear();
+}
+
 function updateGuesses() {
-  const l = Math.min(prevParticles.length, prevSounds.length);
-  if (l === prevGuessL) return;
-  prevGuessL = l;
-
-  /** @type {typeof guessPos} */
-  const guesses = new Map();
-  const particles = prevParticles.slice(0, l);
-  const pitches = prevSounds.slice(0, l);
-
-  const { b: pitchB, a: pitchA } = linReg(pitches.map((v, i) => [i, v.p]));
-  const distanceBackup = Math.E / pitchB;
-
   // from https://github.com/hannibal002/SkyHanni/blob/08e5cf831e3e22401d1de830ee522aadcff6634d/src/main/java/at/hannibal2/skyhanni/utils/PolynomialFitter.kt
-  const splineX = ndRegression(3, particles.map((v, i) => [i, v.x]));
-  const splineY = ndRegression(3, particles.map((v, i) => [i, v.y]));
-  const splineZ = ndRegression(3, particles.map((v, i) => [i, v.z]));
-  splinePoly = [
+  const splineX = ndRegression(3, knownParticleChain.map((v, i) => [i, v.x]));
+  const splineY = ndRegression(3, knownParticleChain.map((v, i) => [i, v.y]));
+  const splineZ = ndRegression(3, knownParticleChain.map((v, i) => [i, v.z]));
+  const _splinePoly = [
     toPolynomial(splineX),
     toPolynomial(splineY),
     toPolynomial(splineZ)
   ];
+  splinePoly.set(_splinePoly);
   const dx0 = splineX[1];
   const dy0 = splineY[1];
   const dz0 = splineZ[1];
@@ -256,125 +254,119 @@ function updateGuesses() {
     )
   ) + 25);
   const weightT = 3 * weight / Math.hypot(dx0, dy0, dz0);
-  const distance = dist(weightT * 1.9, distanceBackup) / distanceBackup > 0.3 ? distanceBackup : weightT * 1.9;
-  // const distance =
-  //   settings.dianaGuessDistanceEstimator === 'Slope' ?
-  //     Math.E / pitchB :
-  //     2836.3513351166325 * pitchA + -1395.7763277125964;
-  // const dist3 = 2924.6641104450428 * pitches[0].p + -1442.1515587278568;
+  const distance = weightT * 1.9;
 
-  {
-    /*
-    const a1 = splineX[3];
-    const a2 = splineY[3];
-    const a3 = splineZ[3];
-    const b1 = splineX[2];
-    const b2 = splineY[2];
-    const b3 = splineZ[2];
-    const c1 = splineX[1];
-    const c2 = splineY[1];
-    const c3 = splineZ[1];
-    const splineIntPoly = toPolynomial([
-      -distance * distance,
-      0,
-      c1 * c1 + c2 * c2 + c3 * c3,
-      2 * (b1 * c1 + b2 * c2 + b3 * c3),
-      2 * (a1 * c1 + a2 * c2 + a3 * c3) + (b1 * b1 + b2 * b2 + b3 * b3),
-      2 * (a1 * b1 + a2 * b2 + a3 * b3),
-      a1 * a1 + a2 * a2 + a3 * a3
-    ]);
-    const splineIntTime = newtonRaphson(splineIntPoly, weightT);
-    */
-    let t = 0;
-    while (t < weightT + 2) {
-      if (
-        (splinePoly[0](t) - splineX[0]) ** 2 +
-        (splinePoly[1](t) - splineY[0]) ** 2 +
-        (splinePoly[2](t) - splineZ[0]) ** 2 >
-        distance * distance
-      ) break;
-      t++;
-    }
-    const splineIntTime = convergeHalfInterval(
-      t => distance - Math.hypot(splinePoly[0](t) - splineX[0], splinePoly[1](t) - splineY[0], splinePoly[2](t) - splineZ[0]),
-      0,
-      t - 1, t,
-      false
-    );
-    guesses.set('Spline', splinePoly.map(v => v(splineIntTime)));
+  /*
+  const a1 = splineX[3];
+  const a2 = splineY[3];
+  const a3 = splineZ[3];
+  const b1 = splineX[2];
+  const b2 = splineY[2];
+  const b3 = splineZ[2];
+  const c1 = splineX[1];
+  const c2 = splineY[1];
+  const c3 = splineZ[1];
+  const splineIntPoly = toPolynomial([
+    -distance * distance,
+    0,
+    c1 * c1 + c2 * c2 + c3 * c3,
+    2 * (b1 * c1 + b2 * c2 + b3 * c3),
+    2 * (a1 * c1 + a2 * c2 + a3 * c3) + (b1 * b1 + b2 * b2 + b3 * b3),
+    2 * (a1 * b1 + a2 * b2 + a3 * b3),
+    a1 * a1 + a2 * a2 + a3 * a3
+  ]);
+  const splineIntTime = newtonRaphson(splineIntPoly, weightT);
+  */
+  /*
+  let t = 0;
+  while (t < weightT + 2) {
+    if (
+      (_splinePoly[0](t) - splineX[0]) ** 2 +
+      (_splinePoly[1](t) - splineY[0]) ** 2 +
+      (_splinePoly[2](t) - splineZ[0]) ** 2 >
+      distance * distance
+    ) break;
+    t++;
   }
-
-  {
-    const poly = createPitchDistPoly(distance);
-    // doesn't work well with large errors + ill fitted
-    // const A = particles.map(v => [1, -2 * v.x, -2 * v.y, -2 * v.z]);
-    // const b = new Array(l).fill(0).map((_, i) => [poly(pitchA + i * pitchB) ** 2 - particles[i].x ** 2 - particles[i].y ** 2 - particles[i].z ** 2]);
-    // const p =
-    //   multMatrix(
-    //     multMatrix(
-    //       invertMatrix(
-    //         multMatrix(
-    //           transposeMatrix(A),
-    //           A
-    //         )
-    //       ),
-    //       transposeMatrix(A)
-    //     ),
-    //     pitchB
-    //   );
-    guesses.set('MLAT', gradientDescent(
-      ([x, y, z]) => -particles.reduce((a, v, i) => a + dist(Math.hypot(v.x - x, v.y - y, v.z - z), poly(pitchA + i * pitchB)), 0),
-      guesses.get('Spline').slice(),
-      [[-500, 500], [-500, 500], [-500, 500]]
-    ));
-  }
-
-  {
-    guesses.set('Bezier', splinePoly.map(v => v(weightT)));
-  }
-
-  guesses.set('Average', geoMedian(Array.from(guesses.values())));
+  const splineIntTime = convergeHalfInterval(
+    t => distance - Math.hypot(_splinePoly[0](t) - splineX[0], _splinePoly[1](t) - splineY[0], _splinePoly[2](t) - splineZ[0]),
+    0,
+    t - 1, t,
+    false
+  );
+  const guess = _splinePoly.map(v => v(splineIntTime));
+  */
+  guess = _splinePoly.map(v => v(weightT));
 
   const _splinePolyPos = new ArrayList();
   for (let i = 0; i <= 100; i++) {
     let t = rescale(i, 0, 100, 0, weightT);
-    _splinePolyPos.add(toArrayList(splinePoly.map(v => v(t))));
+    _splinePolyPos.add(toArrayList(_splinePoly.map(v => v(t))));
   }
   unrun(() => {
     splinePolyPos = _splinePolyPos;
-    guessPos = guesses;
+    guessPos = guess;
   });
 }
-function createPitchDistPoly(dist) {
-  return toPolynomial([
-    0.34672856180294437 * dist + 16.075158197019384,
-    2.49969541740811 * dist + -46.744667774875325,
-    -2.7801642926160226 * dist + 35.819136056204,
-    0.721661786664227 * dist + -8.207684155501738,
-  ]);
-}
 
-const soundPlayReg = reg('packetReceived', pack => {
-  if (pack.func_149212_c() !== 'note.harp') return;
-  if (pack.func_149208_g() !== 1) return;
-  const pitch = pack.func_149209_h();
+function ransac() {
+  const t = getTickCount();
+  possibleStartingParticles.removeIf(v => v.t < t - 80);
+  unclaimedParticles.removeIf(v => v.t < t - 80);
+  const L1 = possibleStartingParticles.length;
+  const L2 = unclaimedParticles.length;
+  const L = L1 + L2;
+  if (L < MIN_CHAIN_LENGTH) return;
 
-  const t = getTickCount() - spadeUseTime;
-  if (prevSounds.length) {
-    const estA = prevSounds[0].p;
-    const estB = Math.E / (2836.3513351166325 * estA + -1395.7763277125964);
-    if (dist((pitch - estA) / (prevSounds.length + 1), estB) / estB > 2 || pitch < estA) {
-      if (t > RESET_THRESH) resetGuess();
-      else return;
-    } else if (t > RESET_THRESH && t - prevSounds[prevSounds.length - 1].t > 5) resetGuess();
+  const comb = binomial(L, MIN_CHAIN_LENGTH);
+  const rand = new Array(L).fill(0).map((_, i) => i);
+  const getPart = i => i < L1 ? possibleStartingParticles.at(i) : unclaimedParticles.at(i - L1);
+
+  for (let i = Math.min(comb, RANSAC_ITERS_PER) - 1; i >= 0; i--) {
+    shuffle(rand, MIN_CHAIN_LENGTH);
+    let possInliersI = rand.slice(0, MIN_CHAIN_LENGTH);
+    let possInliers = possInliersI.map(v => getPart(v));
+    let minT = possInliers.reduce((a, v) => a < v.t ? a : v.t, Number.POSITIVE_INFINITY);
+    let poly = [
+      toPolynomial(ndRegression(3, possInliers.map(v => [v.t - minT, v.x]))),
+      toPolynomial(ndRegression(3, possInliers.map(v => [v.t - minT, v.y]))),
+      toPolynomial(ndRegression(3, possInliers.map(v => [v.t - minT, v.z])))
+    ];
+
+    /** @type {ParticlePos[]} */
+    let inliers = [];
+    let addIf = v => {
+      const px = poly[0](v.t - minT);
+      const py = poly[1](v.t - minT);
+      const pz = poly[2](v.t - minT);
+      if (
+        (v.x - px) ** 2 +
+        (v.y - py) ** 2 +
+        (v.z - pz) ** 2
+        < 25
+      ) inliers.push(v);
+    };
+    possibleStartingParticles.forEach(addIf);
+    unclaimedParticles.forEach(addIf);
+
+    minT = inliers.reduce((a, v) => a < v.t ? a : v.t, Number.POSITIVE_INFINITY);
+    if (Math.min(
+      pearsonCoeff(inliers.map(v => v.t - minT), inliers.map(v => v.x)) ** 2,
+      pearsonCoeff(inliers.map(v => v.t - minT), inliers.map(v => v.y)) ** 2,
+      pearsonCoeff(inliers.map(v => v.t - minT), inliers.map(v => v.z)) ** 2,
+    ) > MIN_CHAIN_PEARSON_SQ) {
+      let s = new Set();
+      inliers.forEach(v => s.add(v.t));
+      possibleStartingParticles.removeIf(v => s.has(v.t));
+      unclaimedParticles.removeIf(v => s.has(v.t));
+      resetGuess();
+      knownParticleChain = inliers.sort((a, b) => a.t - b.t);
+      updateGuesses();
+      break;
+    }
   }
+}
 
-  prevSounds.push({
-    t,
-    p: pitch
-  });
-  updateGuesses();
-}).setFilteredClass(net.minecraft.network.play.server.S29PacketSoundEffect).setEnabled(settings._dianaGuessFromParticles);
 const spawnPartReg = reg('packetReceived', pack => {
   if (!(
     pack.func_179749_a().equals(EnumParticleTypes.DRIP_LAVA) &&
@@ -389,23 +381,48 @@ const spawnPartReg = reg('packetReceived', pack => {
   const x = pack.func_149220_d();
   const y = pack.func_149226_e();
   const z = pack.func_149225_f();
-  const t = getTickCount() - spadeUseTime;
-  if (prevParticles.length) {
-    const pp = prevParticles[prevParticles.length - 1];
-    if ((pp.x - x) ** 2 + (pp.y - y) ** 2 + (pp.z - z) ** 2 > 49) {
-      if (t > RESET_THRESH) resetGuess();
-      else return;
-    } else if (t > RESET_THRESH && t - prevParticles[prevParticles.length - 1].t > 5) resetGuess();
+  const t = getTickCount();
+  const obj = { t, x, y, z };
+  let isKnown = false;
+  if (knownParticleChain.length) {
+    if (t < knownParticleChain[knownParticleChain.length - 1].t + 5) {
+      const spline = splinePoly.get();
+      if (spline) {
+        const predicted = spline.map(v => v(knownParticleChain.length));
+        if ((predicted[0] - x) ** 2 + (predicted[1] - y) ** 2 + (predicted[2] - z) ** 2 < 25) {
+          knownParticleChain.push(obj);
+          isKnown = true;
+          updateGuesses();
+        }
+      }
+    }
   }
 
-  prevParticles.push({
-    t,
-    x,
-    y,
-    z
-  });
-  updateGuesses();
+  if (!isKnown) {
+    if (spadeUsePositions.some(v =>
+      (v[0] - x) ** 2 +
+      (v[1] - y) ** 2 +
+      (v[2] - z) ** 2
+      < 25
+    )) possibleStartingParticles.push(obj);
+    else unclaimedParticles.push(obj);
+
+    ransac();
+  }
 }).setFilteredClass(net.minecraft.network.play.server.S2APacketParticles).setEnabled(settings._dianaGuessFromParticles);
+
+const spadeUseReg = reg('packetSent', pack => {
+  if (pack.func_149568_f() !== 255) return;
+  const stack = pack.func_149574_g();
+  if (!stack) return;
+  const id = getSbId(stack);
+
+  if (id === 'ANCESTRAL_SPADE' || id === 'ARCHAIC_SPADE' || id === 'DEIFIC_SPADE') spadeUsePositions.push([
+    getLastReportedX(),
+    getLastReportedY(),
+    getLastReportedZ()
+  ]);
+}).setFilteredClass(net.minecraft.network.play.client.C08PacketPlayerBlockPlacement).setEnabled(settings._dianaGuessFromParticles);
 
 const renderGuessReg = reg('renderWorld', () => {
   if (splinePolyPos.size()) renderLine(
@@ -413,25 +430,16 @@ const renderGuessReg = reg('renderWorld', () => {
     splinePolyPos,
     { phase: true }
   );
-  guessPos.forEach((v, k) => {
-    if (!v) return;
-    renderBoxOutline(
-      settings[`dianaGuessFromParticles${k}Color`] ?? 0,
-      v[0], v[1] - 1, v[2],
-      1, 1,
-      { phase: true }
-    );
-    if (settings.dianaGuessFromParticlesRenderName) renderBillboardString(
-      settings[`dianaGuessFromParticles${k}Color`] ?? 0,
-      k,
-      v[0], v[1] - 1 + 1.5, v[2],
-      { increase: true, phase: true }
-    );
-  });
+  if (guessPos) renderBoxOutline(
+    settings.dianaGuessFromParticlesAverageColor,
+    guessPos[0], guessPos[1] - 1, guessPos[2],
+    1, 1,
+    { phase: true }
+  );
 }).setEnabled(settings._dianaGuessFromParticles);
 
 const tickReg = reg('tick', () => {
-  let closest = foundGuessBurrow ? null : guessPos.get('Average');
+  let closest = foundGuessBurrow ? null : guessPos;
   let closestD = closest ? (Player.getX() - closest[0]) ** 2 + (Player.getY() - closest[1]) ** 2 + (Player.getZ() - closest[2]) ** 2 : Number.POSITIVE_INFINITY;
   if (Number.isNaN(closestD)) {
     closest = null;
@@ -451,7 +459,8 @@ const tickReg = reg('tick', () => {
   });
   if (i > 0) burrows = burrows.slice(i);
 
-  const isHoldingSpade = getSbId(Player.getHeldItem()) === 'ANCESTRAL_SPADE';
+  const heldId = getSbId(Player.getHeldItem());
+  const isHoldingSpade = heldId === 'ANCESTRAL_SPADE' || heldId == 'ARCHAIC_SPADE' || heldId === 'DEIFIC_SPADE';
   prevGuesses = prevGuesses.filter(v => {
     if (t - v[3] > 5 * 60 * 20) return false;
     const d = (Player.getX() - v[0]) ** 2 + (Player.getY() - v[1]) ** 2 + (Player.getZ() - v[2]) ** 2;
@@ -482,35 +491,33 @@ const startBurrowReg = reg('chat', () => {
   burrowDigReg.register();
   burrowResetReg.register();
   renderTargetsReg.register();
-  soundPlayReg.register();
   spawnPartReg.register();
+  spadeUseReg.register();
   renderGuessReg.register();
   tickReg.register();
   arrowPointReg.register();
   unloadReg.register();
-}).setCriteria('&r&eYou dug out a Griffin Burrow! &r&7(1/4)&r');
+}).setCriteria(/&r&eYou dug out a Griffin Burrow! &r&7\(\d\/\d\)&r/);
 const unloadReg = reg('worldUnload', () => {
   removeClosestReg.unregister();
   burrowSpawnReg.unregister();
   burrowDigReg.unregister();
   burrowResetReg.unregister();
   renderTargetsReg.unregister();
-  soundPlayReg.unregister();
   spawnPartReg.unregister();
+  spadeUseReg.unregister();
   renderGuessReg.unregister();
   tickReg.unregister();
   arrowPointReg.unregister();
   unloadReg.unregister();
 
   targetLoc = null;
-  resetGuess();
-  // prevGuesses = [];
-  // burrows = [];
+  fullReset();
 });
 const burrowResetReg = reg('chat', () => {
+  fullReset();
   unrun(() => {
     targetLoc = null;
-    resetGuess();
     burrows = [];
     prevGuesses = [];
   });
